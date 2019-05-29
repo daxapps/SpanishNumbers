e26695882bc05b87e413362a7d96089e5c1e94a3
Jason Crawford <daxapps777@gmail.com>
2019-05-29 18:13:09 -0400

    MVP

--- App.js
+++ App.js
@@ -1,11 +1,70 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { Audio } from 'expo';
+
+const listBackgroundColors = {
+  1: "#D63031",
+  2: "#67E6DC",
+  3: "#0A79DF",
+  4: "#10A881",
+  5: "#019031",
+  6: "#BA2F16",
+  7: "#FFF222",
+  8: "#758AA2",
+  9: "#BB2CD9",
+  10: "#EA7773"
+}
+
+const soundList = {
+  one: require('./assets/one.wav'),
+  two: require('./assets/two.wav'),
+  three: require('./assets/three.wav'),
+  four: require('./assets/four.wav'),
+  five: require('./assets/five.wav'),
+  six: require('./assets/six.wav'),
+  seven: require('./assets/seven.wav'),
+  eight: require('./assets/eight.wav'),
+  nine: require('./assets/nine.wav'),
+  ten: require('./assets/ten.wav')
+}
 
 export default class App extends React.Component {
+
+  playSound = async number => {
+    const soundObject = new Audio.Sound();
+      try {
+        let path = soundList[number]
+        await soundObject.loadAsync(path)
+        await soundObject
+        .playAsync()
+        .then( async playbackStatus => {
+          setTimeout(() => {
+            soundObject.unloadAsync();
+          }, playbackStatus.playableDurationMillis);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.gridContainer}>
+          <Image style={styles.logo} source={require("./assets/logo.png")} />
+          <View style={styles.rowContainer}>
+            <TouchableOpacity style={[{ backgroundColor: listBackgroundColors[1] }, styles.item]}
+            onPress={() => {
+              this.playSound("one")
+            }}
+            >
+              <Text style={styles.itemText}>One</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     );
   }
@@ -18,4 +77,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  gridContainer: {
+    flex: 1,
+    margin: 5
+  },
+  logo: {
+    alignSelf: "center",
+    marginTop: 15
+  },
+  rowContainer: {
+    flexDirection: "row"
+  },
+  item: {
+    flex: 1,
+    height: 110,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 2
+  },
+  itemText: {
+    color: "white",
+    fontSize: 20
+  }
 });
